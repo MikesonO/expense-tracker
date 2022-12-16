e39667ac548f781332ebe58c85d85efb9f117e76
mikeson <mikeson.jnr@gmail.com>
2022-12-16 23:51:37 +0000

    Refactor code and update ExpenseDate styling

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
       id: 'e1',
       title: 'Car Insurance',
       ammount: 294.67,
-      date: new Date(2022, 12, 15),
+      date: new Date(2023, 11, 31),
     },
     {
       id: 'e2',
       title: 'Toilet Paper',
       ammount: 4.99,
-      date: new Date(2022, 12, 15),
+      date: new Date(2022, 11, 16),
     },
     {
       id: 'e3',
       title: 'Gym Membership',
       ammount: 21.99,
-      date: new Date(2022, 12, 1),
+      date: new Date(2022, 11, 1),
     },
     {
       id: 'e4',
       title: 'New Monitor',
       ammount: 450,
-      date: new Date(2022, 12, 15),
+      date: new Date(2022, 11, 16),
     },
   ];
 
