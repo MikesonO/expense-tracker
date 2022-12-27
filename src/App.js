d8ec7f38eafc40e26bf02deaa06e824205a825bd
mikeson <mikeson.jnr@gmail.com>
2022-12-27 19:53:46 +0000

    Display and update expense list dynamically

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
+import React, { useState } from 'react';
+
 import NewExepense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const TEST_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2023, 11, 31),
+  },
+  {
+    id: 'e2',
+    title: 'Toilet Paper',
+    amount: 4.99,
+    date: new Date(2022, 11, 16),
+  },
+  {
+    id: 'e3',
+    title: 'Gym Membership',
+    amount: 21.99,
+    date: new Date(2022, 11, 1),
+  },
+  {
+    id: 'e4',
+    title: 'New Monitor',
+    amount: 450,
+    date: new Date(2022, 11, 16),
+  },
+];
+
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2023, 11, 31),
-    },
-    {
-      id: 'e2',
-      title: 'Toilet Paper',
-      amount: 4.99,
-      date: new Date(2022, 11, 16),
-    },
-    {
-      id: 'e3',
-      title: 'Gym Membership',
-      amount: 21.99,
-      date: new Date(2022, 11, 1),
-    },
-    {
-      id: 'e4',
-      title: 'New Monitor',
-      amount: 450,
-      date: new Date(2022, 11, 16),
-    },
-  ];
+  const [expenses, setExpenses] = useState(TEST_EXPENSES)
 
   const addExpenseHandler = expense => {
-    console.log('in App JS');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
