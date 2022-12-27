4b3c1bd9da8f166158d3f478134c3ae232d29300
mikeson <mikeson.jnr@gmail.com>
2022-12-27 19:46:53 +0000

    Refactor code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,7 +9,7 @@ const Expenses = (data) => {
 
   const [filteredYear, setFilteredYear] = useState('2020');
 
-  const expense = data.expense;
+  const expenses = data.expense;
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
@@ -19,27 +19,18 @@ const Expenses = (data) => {
   return (
     <div>
       <Card className='expenses'>
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}  />
-       <ExpenseItem
-      title={expense[0].title}
-      amount={expense[0].amount}
-      date={expense[0].date}
+      <ExpenseFilter 
+        selected={filteredYear} 
+        onChangeFilter={filterChangeHandler}  
       />
-     <ExpenseItem
-      title={expense[1].title}
-      amount={expense[1].amount}
-      date={expense[1].date}
-     />
-     <ExpenseItem
-      title={expense[2].title}
-      amount={expense[2].amount}
-      date={expense[2].date}
-     />
-     <ExpenseItem
-      title={expense[3].title}
-      amount={expense[3].amount}
-      date={expense[3].date}
-      />
+
+      {expenses.map((expense) => (
+        <ExpenseItem 
+          title={expense.title} 
+          amount ={expense.amount} 
+          date={expense.date}
+        />
+      ))}
    </Card>
   </div>
   );
