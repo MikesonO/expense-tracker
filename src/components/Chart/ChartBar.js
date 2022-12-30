3dae1435fd8ca4f128f28a4437c341d708b4e076
mikeson <mikeson.jnr@gmail.com>
2022-12-30 15:46:17 +0000

    Add ChartBar component

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,19 @@
+
+const ChartBar = data => {
+  let barFill = '0%';
+
+  if (data.max > 0) {
+    barFill = Math.round((data.value / data.maxValue) * 100) + '%';
+  }
+
+  return (
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        <div className='chart-bar__fill' style={{height: barFill}}></div>
+      </div>
+        <div className='chart-bar__label'>{data.label} </div>
+    </div>
+  )
+}
+
+export default ChartBar;
