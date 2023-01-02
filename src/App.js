77b26392671f8fb68c509170a3ed8cc164d3ebb1
mikeson <mikeson.jnr@gmail.com>
2023-01-02 13:19:39 +0000

    Add localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import NewExepense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
@@ -34,12 +34,35 @@ const TEST_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(TEST_EXPENSES)
 
+  // Local Storage
+  useEffect (()=>{
+    const data = window.localStorage.getItem('SAVED_EXPENSES');
+    if (data !== null) {
+
+      const item = JSON.parse(data, function (key, value) {
+        if (key === 'date') {
+          return new Date(value);
+        } else {
+          return value;
+        }
+      });
+
+      setExpenses(item)
+    } 
+  }, []);
+
+  useEffect (()=>{
+    window.localStorage.setItem('SAVED_EXPENSES', JSON.stringify(expenses))
+  }, [expenses]);
+
+  // Add New Expense to list
   const addExpenseHandler = expense => {
     setExpenses(prevExpenses => {
       return [{id: Math.random().toString(), ...expense}, ...prevExpenses];
     });
   }
 
+  // Delete Expense from list
   const deleteExpenseHandler = expense => {
     setExpenses(expenses.filter(item => item.id !== expense));
   }
